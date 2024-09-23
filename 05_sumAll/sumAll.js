2efc3213cfc6f5b3394ce415df98061490fc24a0
C0de3nchantress <e23bscu0005@bennett.edu.in>
2024-09-23 20:51:54 +0530

    solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    if  ((start < 0 || end < 0) || 
+        (!(typeof start == "number") || !(typeof end == "number")) || 
+        (!(start % 1 == 0) || !(end % 1 == 0))){
+        return "ERROR";
+    }
+
+    if (end < start) {
+        let temp = start;
+        start  = end;
+        end = temp;
+    }
+
+    for (let i = start; i <= end; i++){
+        sum += i;
+    }
+    return sum;
 };
+
+console.log(typeof 2.213);
 
 // Do not edit below this line
 module.exports = sumAll;
