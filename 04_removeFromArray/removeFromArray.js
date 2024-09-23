176315d851a243f51a8811b7bb966aa8a63cb437
C0de3nchantress <e23bscu0005@bennett.edu.in>
2024-09-23 20:36:51 +0530

    solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    for(let i = 1; i < arguments.length; i++) {
+        let val = arguments[i];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === val) {
+                array.splice(i, 1);
+                i -= 1;
+            }
+        }
+    }
+    return array; 
 };
 
 // Do not edit below this line
