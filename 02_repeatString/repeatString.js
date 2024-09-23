1e694379c5c87fc806efc6ac64a2377d5fb60710
C0de3nchantress <e23bscu0005@bennett.edu.in>
2024-09-23 19:47:11 +0530

    solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, numberOfTimes) {
+    finalWord = "";
+    if (numberOfTimes < 0) {
+        return "ERROR";
+    }
+    for (i = 0; i < numberOfTimes; i++) {
+        finalWord += word; 
+    }
+    return finalWord;   
 };
 
 // Do not edit below this line
