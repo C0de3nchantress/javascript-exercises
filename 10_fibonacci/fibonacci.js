5227bd7dfc7c3bced1e52d621a476ac4eadad4cb
C0de3nchantress <e23bscu0005@bennett.edu.in>
2024-09-23 22:27:07 +0530

    solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
+    if (number == 0) return 0;
+    let array = [1, 1];
+    for (let i = 2; i < number; i++) {
+        array.push(array[i - 1] + array[i - 2]);
+    }
+    return array[number - 1];
 };
 
 // Do not edit below this line
