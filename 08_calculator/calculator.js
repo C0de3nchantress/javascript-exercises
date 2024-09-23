e740fefb5e6e5886dae1dc08380d4522e018e916
C0de3nchantress <e23bscu0005@bennett.edu.in>
2024-09-23 21:34:57 +0530

    solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let finalSum = 0;
+  array.forEach(element => finalSum += element);
+
+  return finalSum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let finalSum = array[0];
+  let newArr = array.slice(1, array.length)
+  newArr.forEach(element => finalSum *= element)
 
+  return finalSum;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  
+  let finalSum = 1;
+
+  for (let i = 2; i <= num; i++) {
+    finalSum *= i; 
+  }
+
+  return finalSum;
 };
 
 // Do not edit below this line
