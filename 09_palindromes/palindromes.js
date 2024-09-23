447c16df70a4dc01301d109bc2243256faa1937d
C0de3nchantress <e23bscu0005@bennett.edu.in>
2024-09-23 21:46:09 +0530

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    word = word.replace(/[^a-zA-Z0-9 / /g]/g, "");
+    newWord = "";
+    let i = word.length - 1;
+    while (i >= 0) {
+        newWord += word[i];
+        i--;
+    }
+    return newWord == word;
 };
 
 // Do not edit below this line
