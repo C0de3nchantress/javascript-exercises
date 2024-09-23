65b993e2e0e6cf351469133cc7480fde0af10815
C0de3nchantress <e23bscu0005@bennett.edu.in>
2024-09-23 20:14:18 +0530

    solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let i = word.length - 1;
+    newWord = "";
+    while (i >= 0) {
+        newWord += word[i];
+        i--;
+    }
+    return newWord;
 };
 
 // Do not edit below this line
