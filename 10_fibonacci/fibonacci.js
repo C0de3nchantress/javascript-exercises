8d19b54e172de16c64c343409c7d8c614ab3840e
C0de3nchantress <dhruvdudeja999@gmail.com>
2025-03-14 00:36:02 +0530

    solved fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    arr = [1, 1]
+    if (num == 0){
+        return 0;
+    }
+    if (num <0) {
+        return "OOPS";
+    } 
+    while (arr.length < num){
+        const num1 = arr[arr.length - 1];
+        const num2 = arr[arr.length - 2];
+        arr.push(num1 + num2);
+    }
+    return arr[arr.length - 1];
 };
 
 // Do not edit below this line
