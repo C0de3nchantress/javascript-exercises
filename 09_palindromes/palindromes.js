fa420e917bebce99d33a0f1efb7fb432718947c3
C0de3nchantress <dhruvdudeja999@gmail.com>
2025-03-14 00:25:09 +0530

    solved palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase()
+    string = string.replace(/[^a-z0-9]/g, "")
+    const reversedString = string.split('').reverse().join('');
+    return string ===  reversedString;
 };
 
 // Do not edit below this line
