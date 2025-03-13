5d11eaffb1b9f0a40230fd8ef6f1c36121e380f7
C0de3nchantress <dhruvdudeja999@gmail.com>
2025-03-14 00:57:04 +0530

    solved findTheOldest and completed all exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    currentYear = 2025
+    return array.reduce((oldest, current) => {
+        const currentAge = (current.yearOfDeath || currentYear) - current.yearOfBirth;
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        return currentAge > oldestAge ? current : oldest;
+    });
 };
 
 // Do not edit below this line
